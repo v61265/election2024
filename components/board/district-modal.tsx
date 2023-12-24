04098d07d1e44bfae4c43b881dea09df2606468a
YiHsuan <v61265@gmail.com>
2023-12-24 17:58:24 +0800

    refactor(district-modal): not close when click option

--- components/board/district-modal.tsx
+++ components/board/district-modal.tsx
@@ -132,7 +132,6 @@ export default function DistrictModal({
               key={item.name}
               onClick={() => {
                 handleOnClickItem(item.name);
-                onClose();
               }}
               isActive={district === item.name}
               size='all'
